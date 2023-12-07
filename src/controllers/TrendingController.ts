b078094c075717c1d6711a57caf53e7dbe9d07b3
diegoarff <diegorinconf1@gmail.com>
2023-12-07 17:15:46 -0400

    refactor: add pagination to trending

--- src/controllers/TrendingController.ts
+++ src/controllers/TrendingController.ts
@@ -2,31 +2,35 @@ import { Request, Response } from "express";
 import BaseController from "./BaseController";
 import AxiosInstance from "../config/axios";
 
-
 class TrendingController extends BaseController {
-    getTrendings = async (req: Request, res: Response): Promise<Response> => {
-        const { type, time } = req.params
-        const language = req.query.language || 'en-US'
-        const page = req.query.page || 1
-        try {
-        const trendings = await AxiosInstance.get(`trending/${type}/${time}?language=${language}&page=${page}`)
-        console.log(trendings.data.results[0])
-        const response: any[] | undefined = []
-        trendings.data.results.forEach((trending: any) => {
-            response.push({
-            id: trending.id,
-            name: trending.title? trending.title : trending.name,
-            poster: `https://image.tmdb.org/t/p/w780${trending.poster_path}`,
-            media_type: trending.media_type,
-            adult: trending.adult,
-            })
+  getTrendings = async (req: Request, res: Response): Promise<Response> => {
+    const { type, time } = req.params;
+    const language = req.query.language || "en-US";
+    const page = req.query.page || 1;
+    try {
+      const trendings = await AxiosInstance.get(
+        `trending/${type}/${time}?language=${language}&page=${page}`
+      );
+      const results: any[] | undefined = [];
+      trendings.data.results.forEach((trending: any) => {
+        results.push({
+          id: trending.id,
+          name: trending.title ? trending.title : trending.name,
+          poster: `https://image.tmdb.org/t/p/w780${trending.poster_path}`,
+          media_type: trending.media_type,
+          adult: trending.adult,
         });
-        return this.successRes(res, 201, 'Success getting data', response)
+      });
+      const response = {
+        page: trendings.data.page,
+        total_pages: trendings.data.total_pages,
+        results,
+      };
+      return this.successRes(res, 201, "Success getting data", response);
     } catch (error) {
-       return this.errorRes(res, 500, 'Internal server error', error) 
-    }    
+      return this.errorRes(res, 500, "Internal server error", error);
     }
-
+  };
 }
 
 export default new TrendingController();
