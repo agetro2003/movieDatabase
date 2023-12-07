ec9a596a8894f51905d2abf482a82fc939d3e1a4
agetro2003 <jesusdaniolob@gmail.com>
2023-12-06 23:33:23 -0400

    Add conditional to filter by media_type

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -24,6 +24,7 @@ const response = search.data.results.filter((result: any) => result.media_type !
 }
 
 searchFilter = async (req: Request, res: Response): Promise<Response> => {
+    const media_type = req.query.media_type || "all";
     const with_genre = req.query.with_genre || null;
     const sort_by = req.query.sort_by || null;
     const primary_release_year = req.query.primary_release_year || null;
@@ -38,35 +39,58 @@ searchFilter = async (req: Request, res: Response): Promise<Response> => {
             query.concat(`&${key}=${value}`)
         }
     }
-    try {
-        const search = await AxiosInstance.get(`discover/movie?${query.slice(1)}`)
-        const search2 = await AxiosInstance.get(`discover/tv?${query.slice(1)}`)
-
-        const response: any[] | undefined = []
-        search.data.results.forEach((result: any) => {
-            response.push({
-                id: result.id,
-                name: result.title,
-                poster: `https://image.tmdb.org/t/p/w780${result.poster_path}`,
-                media_type: 'movie',
-                adult: result.adult,
+    if (media_type == "all") {
+        try {
+            const search = await AxiosInstance.get(`discover/movie?${query.slice(1)}`)
+            const search2 = await AxiosInstance.get(`discover/tv?${query.slice(1)}`)
+    
+            const response: any[] | undefined = []
+            search.data.results.forEach((result: any) => {
+                response.push({
+                    id: result.id,
+                    name: result.title,
+                    poster: `https://image.tmdb.org/t/p/w780${result.poster_path}`,
+                    media_type: 'movie',
+                    adult: result.adult,
+                })
             })
-        })
-        search2.data.results.forEach((result: any) => {
-            response.push({
-                id: result.id,
-                name: result.name,
-                poster: `https://image.tmdb.org/t/p/w780${result.poster_path}`,
-                media_type: 'tv',
-                adult: result.adult,
+            search2.data.results.forEach((result: any) => {
+                response.push({
+                    id: result.id,
+                    name: result.name,
+                    poster: `https://image.tmdb.org/t/p/w780${result.poster_path}`,
+                    media_type: 'tv',
+                    adult: result.adult,
+                })
             })
-        })
-
-        return this.successRes(res, 201, 'Success getting data', response)
-    } catch (error) {
-        return this.errorRes(res, 500, 'Internal server error', error) 
-
+    
+            return this.successRes(res, 201, 'Success getting data', response)
+        } catch (error) {
+            return this.errorRes(res, 500, 'Internal server error', error) 
+    
+        }
+    } else {
+        try {
+            const search = await AxiosInstance.get(`discover/${media_type}?${query.slice(1)}`)    
+            const response: any[] | undefined = []
+            search.data.results.forEach((result: any) => {
+                response.push({
+                    id: result.id,
+                    name: result.title,
+                    poster: `https://image.tmdb.org/t/p/w780${result.poster_path}`,
+                    media_type: 'movie',
+                    adult: result.adult,
+                })
+            })
+          
+    
+            return this.successRes(res, 201, 'Success getting data', response)
+        } catch (error) {
+            return this.errorRes(res, 500, 'Internal server error', error) 
+    
+        }
     }
+   
     
 }
 }
