3b7c47630377a766c9f0c16cb9918447545a1d1c
diegoarff <diegorinconf1@gmail.com>
2023-12-15 20:44:26 -0400

    feat: fully functional chat rooms

--- src/controllers/SocketController.ts
+++ src/controllers/SocketController.ts
@@ -2,37 +2,39 @@ import { Socket } from "socket.io";
 import { Message, User } from "../models";
 
 class SocketController {
-    connection = (socket: Socket)=> {
-        socket.on('join-chat', (data) =>{
-            const {userId, chatId} = data
-            socket.join(chatId)
-            console.log('user connected', userId)
-            socket.broadcast.to(chatId).emit('user-connected', userId)
-       
-        })
-        socket.on('send-message', async (data)=>{
-            const {content, userId, chatId} = data
-                console.log(content)
-            const message = new Message({
-                content: content,
-                userId: userId,
-                chatId: chatId
-            })
-            await message.save()
-            const user = await User.findById(userId)
-            console.log(message)
-            socket.broadcast.to(chatId).emit('receive-message', {content, user})
-            })
+  connection = (socket: Socket) => {
 
-            
-            socket.on('disconnect', ()=>{
-                console.log('user disconnected')
-7            })
-    }
+    socket.on("join-chat", (data) => {
+      const { userId, chatId } = data;
+      socket.join(chatId);
+      socket.broadcast.to(chatId).emit("user-connected", userId);
+    });
 
+    socket.on("leave-chat", (data) => {
+      const { userId, chatId } = data;
+      socket.leave(chatId);
+      socket.broadcast.to(chatId).emit("user-disconnected", userId);
+    });
 
+    socket.on("send-message", async (data) => {
+      const { content, userId, chatId } = data;
 
+      const message = new Message({
+        content: content,
+        userId: userId,
+        chatId: chatId,
+      });
 
+      await message.save();
+      const user = await User.findOne({ _id: userId });
+
+      socket.broadcast.to(chatId).emit("receive-message", { content, userId: user?.toSafeObject()});
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  };
 }
 
-export default new SocketController()
+export default new SocketController();
