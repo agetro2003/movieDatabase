069b806f8ac1a6ce6227e4f9307038b4858107e6
diegoarff <diegorinconf1@gmail.com>
2023-12-15 22:08:06 -0400

    Delete user messages on account deletion

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,21 @@
 import { Schema, model } from "mongoose";
 import { type IUserDocument } from "../interfaces";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import Review from "./Review";
 import Comment from "./Comment";
+import Message from "./Message";
 
 const UserSchema = new Schema<IUserDocument>(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, require: true },
-    avatar: { type: String, default: "https://firebasestorage.googleapis.com/v0/b/ramble-322a6.appspot.com/o/test%2F1698975678282.jpeg?alt=media&token=c9a7b759-be64-4a3e-aa5b-b932c7b7159f" },
+    avatar: {
+      type: String,
+      default:
+        "https://firebasestorage.googleapis.com/v0/b/ramble-322a6.appspot.com/o/test%2F1698975678282.jpeg?alt=media&token=c9a7b759-be64-4a3e-aa5b-b932c7b7159f",
+    },
     critic: { type: Boolean, default: false },
   },
   {
@@ -19,31 +24,30 @@ const UserSchema = new Schema<IUserDocument>(
   }
 );
 
-
-
 UserSchema.pre<IUserDocument>("save", async function (next) {
-  if (!this.isModified('password')) next();
+  if (!this.isModified("password")) next();
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(this.password, salt);
 
   this.password = hashedPassword;
   next();
-}) 
+});
 
-UserSchema.pre('deleteOne', { document: true }, async function (next) {
+UserSchema.pre("deleteOne", { document: true }, async function (next) {
   const userId = this._id;
 
   await Promise.all([
     Review.deleteMany({ userId }),
     Comment.deleteMany({ userId }),
+    Message.deleteMany({ userId }),
   ]);
 
   next();
 });
 
 UserSchema.methods.comparePassword = async function (
-  password: string,
+  password: string
 ): Promise<boolean> {
   return await bcrypt.compare(password, this.password);
 };
@@ -51,7 +55,7 @@ UserSchema.methods.comparePassword = async function (
 UserSchema.methods.createToken = function (): string {
   return jwt.sign(
     { id: this._id, username: this.username },
-    (process.env.JWT_SECRET as string) || 'secret',
+    (process.env.JWT_SECRET as string) || "secret"
   );
 };
 
