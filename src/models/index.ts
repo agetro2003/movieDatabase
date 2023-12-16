d4aef0d3db7a34a417d766e2cee4b5b6ca87289c
diegoarff <diegorinconf1@gmail.com>
2023-12-16 01:32:44 -0400

    try again. amen

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,8 @@
-import Chat from "./Chat";
+import Chat from "./chat";
 import Media from "./Media";
-import Review from "./Review";
-import Message from "./Message";
+import Review from "./review";
+import Message from "./message";
 import Comment from "./Comment";
-import User from "./User";
+import User from "./user";
 
 export { Chat, Media, Review, Message, Comment, User };
